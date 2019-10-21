5914a5b57329a1429101ea5f28f324ceef107330
Juliano <julianotrabalho2016@gmail.com>
2019-10-21 13:25:27 -0300

    UPDATE - created file list page

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
-
+import { MdInsertDriveFile } from "react-icons/md";
+import api from "../../services/api";
 import "./styles.css";
 
 export default class Box extends Component {
+  state = {
+    box: ""
+  };
+
+  async componentDidMount() {
+    const box = this.props.match.params.id;
+    const response = await api.get(`boxes/${box}`);
+    this.setState({
+      box: response.data
+    });
+  }
+
   render() {
-    return <div />;
+    const { box } = this.state;
+    return (
+      <div id="box-container">
+        <header>
+          <h1>Dropbox</h1>
+          <h2>{box.title}</h2>
+        </header>
+        <ul>
+          {box.files &&
+            box.files.map(file => (
+              <li>
+                <a className="fileInfo" href={file.url} target="_blank">
+                  <MdInsertDriveFile size={24} color="#a5cfff" />
+                  <strong>{file.title}</strong>
+                </a>
+                <span>há {file.createdAt}</span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
   }
 }
