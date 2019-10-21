2a27e78d4e917484d068cee9687943a33c1803c2
Juliano <julianotrabalho2016@gmail.com>
2019-10-21 11:24:12 -0300

    UPDATE - finished page create box

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import "./styles.css";
+import api from "../../services/api";
 
 export default class Main extends Component {
+  state = {
+    newBox: ""
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const { newBox } = this.state;
+    const response = await api.post("boxes", {
+      title: newBox
+    });
+    console.log(response.data);
+  };
+
+  handleChange = e => {
+    this.setState({ newBox: e.target.value });
+  };
+
   render() {
+    const { newBox } = this.state;
     return (
       <div id="main-container">
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <h1>Dropbox</h1>
-          <input type="text" placeholder="Criar um box" />
+          <input
+            type="text"
+            placeholder="Criar um box"
+            onChange={this.handleChange}
+            value={newBox}
+          />
           <button type="submit">Criar</button>
         </form>
       </div>
